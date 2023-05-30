a5aaae208d8b3faf49b6a21cc97076e401693712
Maltsev Nikita <maltsew.na@gmail.com>
2023-05-30 15:00:07 +0300

    Добавлен файл настроек BaseSettings для приложения

---
+++ settings.py
@@ -0,0 +1,19 @@
+"""
+Настройки и переменные окружения
+"""
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    server_host: str = '127.0.0.1'
+    server_port: int = 8000
+    database_url: str = 'postgresql+asyncpg://fastapi:a2095a2095@0.0.0.0:5432/fastapi_app'
+
+
+# settings = Settings(
+#     _env_file='.env',
+#     _env_file_encoding='utf-8'
+# )
+
+
+
