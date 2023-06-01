63934b1e2405eb2bac4c7c1157e655ac56f0e451
Maltsev Nikita <maltsew.na@gmail.com>
2023-06-01 17:27:01 +0300

    Добавлены эндпоинты: Получение списка всех user Получение user по user_id

--- task_manager/api/users.py
+++ task_manager/api/users.py
@@ -1,10 +1,30 @@
+from typing import List, Union
+
+from fastapi import Depends
 from fastapi import APIRouter
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from ..db.middleware import get_session
+from ..models.users import User
+from ..db.tables import User as user
 
 router = APIRouter(
     prefix='/users',
 )
 
 
-@router.get('/')
-def get_users():
-    return []
+@router.get('/', response_model=List[User])
+async def get_users(session: Session = Depends(get_session)):
+    # style 2.0
+    query = select(user)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+@router.get('/{user_id}', response_model=User)
+async def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
+    query = select(user).filter(user.user_id == user_id)
+    result = await session.execute(query)
+    return result.scalars().first()
