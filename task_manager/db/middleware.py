14aaafcd7df9bf9e879c96c3c73514bd35037515
Maltsev Nikita <maltsew.na@gmail.com>
2023-06-01 17:10:05 +0300

    Добавлена инициализация движка базы данных

--- task_manager/db/middleware.py
+++ task_manager/db/middleware.py
@@ -1,15 +1,21 @@
 """
 Организация взаимодейтсвия между базой данных и приложением
 """
+import sys
+import pathlib
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from tables import Base
+from .tables import Base
+
+# sys.path.append(pathlib.Path(__file__).resolve().parents[0])
+# print(sys.path)
 
 
-from settings import DATABASE_URL
+from ..settings import settings
 
 # create engine
-db_engine = create_async_engine(DATABASE_URL, echo=True)
+db_engine = create_async_engine(settings.database_url, echo=True)
 
 # создание сессии
 async_session = sessionmaker(db_engine, expire_on_commit=False, class_=AsyncSession)
