37071e232d26ee7fe3d615e8d77d79865e9eb460
Maltsev Nikita <maltsew.na@gmail.com>
2023-06-03 15:26:57 +0300

    Добавлен эндроинт для создания новой задачи

--- task_manager/api/tasks.py
+++ task_manager/api/tasks.py
@@ -6,7 +6,7 @@ from typing import List, Union
 from fastapi import Depends
 from fastapi import APIRouter
 
-from ..models.tasks import Task
+from ..models.tasks import Task, ShowTask, CreateTask
 from ..services.tasks import TasksService
 
 
@@ -15,6 +15,13 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model=None)
+@router.get('/', response_model=List[ShowTask])
 async def get_tasks(service: TasksService = Depends()):
+    """ Получить список всех задач"""
     return await service.get_all_tasks()
+
+
+@router.post('/', response_model=None)
+async def create_task(task_body: CreateTask, service: TasksService = Depends()):
+    """ Создать задачу"""
+    return await service.create_task(title=task_body.title, description=task_body.description, assign_to_id=task_body.assign_to_id, status=task_body.status)
