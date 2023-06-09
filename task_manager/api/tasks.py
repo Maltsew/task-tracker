969e52c376ea7df96833fe9a2d508c570ea02bc4
Maltsev Nikita <maltsew.na@gmail.com>
2023-06-09 13:30:33 +0300

    Получить задачу по id

--- task_manager/api/tasks.py
+++ task_manager/api/tasks.py
@@ -21,7 +21,7 @@ async def get_tasks(service: TasksService = Depends()):
     return await service.get_all_tasks()
 
 
-@router.post('/', response_model=None)
+@router.post('/', response_model=ShowTask)
 async def create_task(task_body: CreateTask, service: TasksService = Depends()):
     """ Создать задачу"""
     return await service.create_task(
@@ -30,3 +30,9 @@ async def create_task(task_body: CreateTask, service: TasksService = Depends()):
         assign_to_id=task_body.assign_to_id,
         status=task_body.status
     )
+
+
+@router.get('/{task_id}', response_model=ShowTask)
+async def get_task_by_id(task_id: int, service: TasksService = Depends()):
+    """ Получить задачу по id"""
+    return await service.get_task_by_id(task_id)
--- task_manager/services/tasks.py
+++ task_manager/services/tasks.py
@@ -37,8 +37,20 @@ class TasksService:
         return result
 
     async def create_task(self, title: str, description: str, assign_to_id: int, status: str) -> ShowTask:
-        """ Создать новую задачу"""
+        """ Создать новую задачу
+        Возвращает созданную задачу
+        """
         create_task = task(title=title, description=description, assign_to_id=assign_to_id, status=status)
         self.session.add(create_task)
         await self.session.commit()
         return create_task
+
+    async def get_task_by_id(self, task_id: int) -> ShowTask:
+        """ Получить задачу по id
+        """
+        query = select(task).filter(task.task_id == task_id)
+        result = await self.session.execute(query)
+        get_task_by_id = result.scalars().first()
+        if not get_task_by_id:
+            raise HTTPException(status_code=404, detail='Tasks not found')
+        return get_task_by_id
